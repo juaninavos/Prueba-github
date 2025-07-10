6ec213a647669c5c85b1c82f752c51d3542cd035
JuaniNavos <juaninavos@gmail.com>
2025-07-10 16:40:17 -0300

    Segundo  commit

--- Prueba github.js
+++ Prueba github.js
@@ -1,1 +1,7 @@
 "Programa prueba github"
+console.log("Hola, este es un programa de prueba para GitHub.");
+console.log("Este es un mensaje de prueba para verificar el funcionamiento de GitHub.");
+console.log("Asegúrate de que todo esté funcionando correctamente.");
+console.log("Si ves este mensaje, todo está bien configurado.");
+console.log("¡Gracias por usar GitHub!");
+console.log("Fin del programa de prueba.");
